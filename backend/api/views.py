290395615666d051f41d42398e4ba2917360a149
Александр Солодников <solodnikov@yandex.ru>
2023-02-26 23:14:56 +0300

    Tag api begining, next urls router

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework import viewsets
+from recipes.models import Tag
+from .serializers import TagSerializer
 
-# Create your views here.
+
+class ListRetrieveViewSet(mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin,
+                          viewsets.GenericViewSet):
+    pass
+
+
+class TagViewSet(ListRetrieveViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
