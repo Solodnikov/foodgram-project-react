a8fe030544e22694c12aa660d99b0e4a5debe7c6
Александр Солодников <solodnikov@yandex.ru>
2023-03-05 22:24:40 +0300

    Loors like get recipe ready

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,7 @@
-from recipes.models import (CustomUser, Favourite, Ingredient,
-                            IngredientsinRecipt, Recipe, Shopping_list, Tag)
+from recipes.models import (Favourite, Ingredient, IngredientsinRecipt, Recipe,
+                            Shopping_list, Tag)
 from rest_framework import serializers
-from users.models import CustomUser, Subscribe
+# from users.models import CustomUser, Subscribe
 from users.serializers import CustomUserSerializer
 
 
@@ -24,23 +24,16 @@ class IngredientsinReciptSerializer(serializers.ModelSerializer):
 
 
 class IngredientSerializer(serializers.ModelSerializer):
-    # amount = IngredientsinRecipt(read_only=True)
-    # amount = serializers.SerializerMethodField()
 
     class Meta:
         model = Ingredient
-        fields = (
-            'id', 'name', 'measurement_unit', 'recipes'
-            # 'amount',
-        )
-    # def get_amount(self,obg):
+        fields = ('id', 'name', 'measurement_unit')
 
 
 class FavouriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favourite
         fields = ('user', 'recipe')
-        # read_only_fields = ('user')
 
 
 class ShoppingSerializer(serializers.ModelSerializer):
@@ -49,55 +42,13 @@ class ShoppingSerializer(serializers.ModelSerializer):
         fields = ('user', 'recipe')
 
 
-# class CustomUserSerializer(serializers.ModelSerializer):
-#     is_subscribed = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = CustomUser
-#         fields = (
-#             'email', 'username', 'first_name',
-#             'first_name', 'is_subscribed',
-#         )
-        # read_only_fields = ('id',)
-
-    # def get_is_subscribed(self, obj):
-    #     request_user = self.context.get('request').user
-    #     return Subscribe.objects.filter(
-    #         subscriber=request_user,
-    #         subscribing=obj
-    #     ).exists()
-
-
 class RecipeSerialiser(serializers.ModelSerializer):
 
     tags = TagSerializer(many=True, read_only=True, required=False)
-        # "id": 28839286,
-        # "name": "Завтрак",
-        # "color": "#E26C2D",
-        # "slug": "breakfast"
     author = CustomUserSerializer(read_only=True)
-        # "username": "Pn1Wz",
-        # "email": "2nvuYmL-1AS@JzG.qci",
-        # "id": 1985130,
-        # "first_name": "Вася",
-        # "last_name": "Пупкин",
-        # "is_subscribed": false
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
-    # ingredients = serializers.StringRelatedField(many=True, read_only=True)
-    # ingredients = IngredientSerializer(many=True, read_only=True)
     ingredients = serializers.SerializerMethodField()
-    
-    # ingredients = IngredientsinReciptSerializer(
-    #     source='ingredientsinrecipt_set',
-    #     many=True,
-    #     read_only=True,
-    # )
-    #   "name": "Картофель отварной",
-    #   "measurement_unit": "г",
-    #   "id": -55850568,
-    #   "amount": 3507066
-
 
     class Meta:
         model = Recipe
