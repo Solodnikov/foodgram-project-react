8f18df940fc5c23d5b26a7a3493c7f1bc305e63c
Александр Солодников <solodnikov@yandex.ru>
2023-03-13 01:25:23 +0300

    Подготовка к 1 ревью

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -17,22 +17,23 @@ class RecipeFilter(FilterSet):
         queryset=Tag.objects.all(),
     )
 
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart')
+    is_favorited = filters.NumberFilter(
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = filters.NumberFilter(
+        method='get_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags']
+        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
 
-    # def filter_is_favorited(self, queryset, name, value):
-    #     user = self.request.user
-    #     if value and not user.is_anonymous:
-    #         return queryset.filter(favorites__user=user)
-    #     return queryset
+    def get_is_favorited(self, queryset, name, value):
+        if value:
+            return queryset.filter(favourites__user=self.request.user)
+        return queryset
 
-    # def filter_is_in_shopping_cart(self, queryset, name, value):
-    #     user = self.request.user
-    #     if value and not user.is_anonymous:
-    #         return queryset.filter(shopping_cart__user=user)
-    #     return queryset
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return queryset.filter(shopping_list__user=self.request.user)
+        return queryset
