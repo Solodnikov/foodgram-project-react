57cdbe1485be0a0b552a898b1cccd56f13357672
Солодников Александр <solodnikov@lanit.ru>
2023-03-28 22:30:37 +0300

    Deploy to server

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '*', ]
+ALLOWED_HOSTS = ['localhost', '*', '84.201.136.137']
 
 
 # Application definition
